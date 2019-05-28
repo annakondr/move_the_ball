017027ae3720154cabacc06a2fb60253746542ed
annakondr <tyukondra@gmail.com>
2019-05-28 22:43:09 +0300

    solution3

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
 'use strict';
+
 const field = document.querySelector('.field');
 const ball = field.querySelector('.ball');
-const fieldY = field.clientHeight - 20;
-const fieldX = field.clientWidth - 20;
+const fieldCoord = field.getBoundingClientRect();
+const fieldY = fieldCoord.y + field.clientTop;
+const fieldX = fieldCoord.x + field.clientLeft;
 
 field.addEventListener('click', function(event) {
-    let moveX = event.offsetX - ball.clientWidth/2;
-    let moveY = event.offsetY - ball.clientHeight/2;
+    let moveX = event.clientX - fieldX - ball.offsetWidth/2;
+    let moveY = event.clientY - fieldY - ball.offsetHeight/2;
+    let fieldRight = field.clientWidth - ball.offsetWidth;
+    let fieldBottom = field.clientHeight - ball.offsetHeight;
 
     if (moveX < 0) {
         moveX = 0;
     }
+    if (moveX > fieldRight) {
+        moveX = fieldRight;
+    }
     if (moveY < 0) {
         moveY = 0;
     }
-    if (moveX > fieldX) {
-        moveX = fieldX - ball.clientWidth/2;
+    if (moveY > fieldBottom) {
+        moveY = fieldBottom;
     }
-    if (moveY > fieldY) {
-        moveY = fieldY - ball.clientHeight/2;
-    }
-    ball.style.left = moveX +'px';
-    ball.style.top = moveY +'px';
+
+    ball.style.left = `${moveX}px`;
+    ball.style.top = `${moveY}px`;
 });
